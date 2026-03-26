6995e437c1e9193974554c153d43e5ca69fc1f7b
AlnnaAntoC <alnnaofficial25@gmail.com>
2026-03-26 10:47:49 +0300

    fourth

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="daniel-stephen">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
